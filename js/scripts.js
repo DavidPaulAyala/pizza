bb8d88787cb3ccc708541bff13dae9fa915a2f47
David Ayala <dpayala@hotmail.com>
2016-08-26 09:55:24 -0700

    add a pizza constructor and display results from input on homepage

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
+//business logic
+function Pizza(sizePie, toppings) {
+  this.sizePie = sizePie;
+  this.toppings = toppings;
+
+}
+
+Pizza.prototype.cost = function() {
+//size of pie price + toppings price;
+
+}
+
+
+
 $(document).ready(function() {
-  event.preventDefault();
+  $("form#new-pizza").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedSize = $("input#new-size").val();
+    var inputtedToppings = $("input#toppings").val();
+
+    var newPizza = new Pizza(inputtedSize, inputtedToppings);
+
+    $("ul#pizzas").append("<li><span class='pizza'>" + newPizza.sizePie + "</span></li>");
+
+    $("input#new-pizza").val("");
+    $("input#toppings").val("");
+  });
 });
